182d12141e247b58f80be1762d05990e47771fa8
MagRaj000 <mrajtor@visa.com>
2025-03-31 11:56:52 +0200

    first commit

---
+++ src/main/java/pl/coderslab/crochetproject/CrochetProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.crochetproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrochetProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrochetProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/coderslab/crochetproject/CrochetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.crochetproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrochetProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
